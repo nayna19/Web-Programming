e6790a634b3a03ae808aa82d98516414b5e1f271
nayna19 <ninanursalsa@gmail.com>
2021-09-17 16:12:03 +0700

    first commit

---
+++ contoh02.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Contoh Scrip PHP</title>
+</head>
+<body>
+<?php
+    $nim = "12170829";
+    $nama = "Bima Bintang Galaxy";
+    $kelas ="12.1A.01";
+
+    echo "Nim Saya   = $nim<br>";
+    echo "Nama Saya  = $nama<br>";
+    echo "Kelas Saya = $kelas<br>";
+?>
+</body>
+</html>
